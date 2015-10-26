dd31a24dee9331983a0188a53ebc3083dcbd034d
Warnar Boekkooi <warnar@cerbex.com>
2015-10-26 12:27:01 +0800

    Make Tokens work without patching CS-Fixer They should have used new static not new self.

--- src/Tokenizer/Tokens.php
+++ src/Tokenizer/Tokens.php
@@ -14,4 +14,75 @@ class Tokens extends BaseTokens
 
         $this[$index]->report($message);
     }
+
+
+    /**
+     * Create token collection from array.
+     *
+     * @param array $array       the array to import
+     * @param bool  $saveIndexes save the numeric indexes used in the original array, default is yes
+     *
+     * @return Tokens
+     */
+    public static function fromArray($array, $saveIndexes = null)
+    {
+        /** @var Tokens $tokens */
+        $tokens = new static(count($array));
+
+        if (null === $saveIndexes || $saveIndexes) {
+            foreach ($array as $key => $val) {
+                $tokens[$key] = $val;
+            }
+
+            return $tokens;
+        }
+
+        $index = 0;
+
+        foreach ($array as $val) {
+            $tokens[$index++] = $val;
+        }
+
+        return $tokens;
+    }
+
+    /**
+     * Create token collection directly from code.
+     *
+     * @param string $code PHP code
+     *
+     * @return Tokens
+     */
+    public static function fromCode($code)
+    {
+        $calculateCodeHashRefl = new \ReflectionMethod(BaseTokens::class, 'calculateCodeHash');
+        $calculateCodeHashRefl->setAccessible(true);
+        $hasCacheRefl = new \ReflectionMethod(BaseTokens::class, 'hasCache');
+        $hasCacheRefl->setAccessible(true);
+        $getCacheRefl = new \ReflectionMethod(BaseTokens::class, 'getCache');
+        $getCacheRefl->setAccessible(true);
+
+        $codeHash = $calculateCodeHashRefl->invoke(null, $code);
+
+        if ($hasCacheRefl->invoke(null, $codeHash)) {
+            /** @var Tokens $tokens */
+            $tokens = $getCacheRefl->invoke(null, $codeHash);
+
+            // generate the code to recalculate the hash
+            $tokens->generateCode();
+
+            if ($codeHash === $tokens->getCodeHash()) {
+                $tokens->clearEmptyTokens();
+                $tokens->clearChanged();
+
+                return $tokens;
+            }
+        }
+
+        $tokens = new static();
+        $tokens->setCode($code);
+        $tokens->clearChanged();
+
+        return $tokens;
+    }
 }
